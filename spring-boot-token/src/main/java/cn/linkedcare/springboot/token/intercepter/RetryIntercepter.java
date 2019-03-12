1e2524a3e72f33f568bee1bbaccf0e35402b1456
wl <164805299@qq.com>
2019-03-12 14:58:00 +0800

    init

---
+++ spring-boot-token/src/main/java/cn/linkedcare/springboot/token/intercepter/RetryIntercepter.java
@@ -0,0 +1,32 @@
+package cn.linkedcare.springboot.token.intercepter;
+
+import java.io.IOException;
+
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+@Slf4j
+public class RetryIntercepter implements Interceptor {
+
+    public int maxRetry;//最大重试次数
+    private int retryNum = 0;//假如设置为3次重试的话，则最大可能请求4次（默认1次+3次重试）
+
+    public RetryIntercepter(int maxRetry) {
+        this.maxRetry = maxRetry;
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();
+        Response response = chain.proceed(request);
+        while (!response.isSuccessful() && retryNum < maxRetry) {
+            retryNum++;
+            
+            log.info("retryNum=" + retryNum);
+            response = chain.proceed(request);
+        }
+        return response;
+    }
+}
