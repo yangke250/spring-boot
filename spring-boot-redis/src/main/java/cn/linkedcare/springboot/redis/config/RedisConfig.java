50e1ae8b4335557bd0cbb7029a61c7b623794743
wl <164805299@qq.com>
2019-03-14 11:24:54 +0800

    redis相关

--- spring-boot-redis/src/main/java/cn/linkedcare/springboot/redis/config/RedisConfig.java
+++ spring-boot-redis/src/main/java/cn/linkedcare/springboot/redis/config/RedisConfig.java
@@ -18,12 +18,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 import cn.linkedcare.springboot.redis.Launch;
-import cn.linkedcare.springboot.redis.constant.RedisConstant;
+import cn.linkedcare.springboot.redis.config.RedisConfig.RedisType;
 import cn.linkedcare.springboot.redis.template.RwSplitRedisTemplate;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -33,11 +34,65 @@ import redis.clients.util.Pool;
 
 
 @Configurable
+@Order
 public class RedisConfig {
 	
 	public static final Logger log = LoggerFactory.getLogger(RedisConfig.class);
 	
-
+	private static String redisUrl;
+	
+	private static int redisDb;
+	
+	private static String redisPassword;
+	
+	private static int redisTimeout;
+	
+	private static RedisType redisType;
+	
+	
+	public static String getRedisUrl() {
+		return redisUrl;
+	}
+	@Value("${redis.url}")
+	public void setRedisUrl(String redisUrl) {
+		RedisConfig.redisUrl = redisUrl;
+	}
+	
+	public static int getRedisDb() {
+		return redisDb;
+	}
+	
+	@Value("${redis.db}")
+	public void setRedisDb(int redisDb) {
+		RedisConfig.redisDb = redisDb;
+	}
+	
+	public static String getRedisPassword() {
+		return redisPassword;
+	}
+	
+	@Value("${redis.password}")
+	public void setRedisPassword(String redisPassword) {
+		RedisConfig.redisPassword = redisPassword;
+	}
+	
+	public static int getRedisTimeout() {
+		return redisTimeout;
+	}
+	
+	@Value("${redis.timeout}")
+	public void setRedisTimeout(int redisTimeout) {
+		RedisConfig.redisTimeout = redisTimeout;
+	}
+	
+	
+	public static RedisType getRedisType() {
+		return redisType;
+	}
+	@Value("${redis.type}")
+	public void setRedisType(RedisType redisType) {
+		RedisConfig.redisType = redisType;
+	}
 
 	
 	public static enum RedisType{
@@ -53,20 +108,17 @@ public class RedisConfig {
 		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
 		jedisPoolConfig.setMaxTotal(50);
 		jedisPoolConfig.setMaxWaitMillis(2000);
-		String redisPassword = null;
-		String redisUrl = RedisConstant.getRedisUrl();
-		int redisDb = RedisConstant.getRedisDb();
-		int redisTimeout = RedisConstant.getRedisTimeout();
+	
 		
 		log.info("=========================================");
 		log.info("=============spring redis host==========="+redisUrl+"===="+redisPassword);
 
 		
 		//不为空的时候设置值
-		if(!StringUtils.isEmpty(RedisConstant.getRedisPassword())){
-			redisPassword=RedisConstant.getRedisPassword();
+		if(StringUtils.isEmpty(redisPassword)){
+			redisPassword=null;
 		}
-		if(RedisType.sentinel==RedisConstant.getRedisType()){
+		if(RedisType.sentinel==redisType){
 			Set<String> sentinels = new HashSet<>(Arrays.asList(redisUrl.split(",")));
 	        // 创建连接池
 	        JedisSentinelPool pool = new JedisSentinelPool(REDIS_NAME,
@@ -88,6 +140,7 @@ public class RedisConfig {
 
 	@Bean
 	@ConditionalOnMissingBean(RwSplitRedisTemplate.class)
+	@Order
 	private RwSplitRedisTemplate createRwSplitRedisTemplate(){
 		RwSplitRedisTemplate rwSplitRedisTemplate = new RwSplitRedisTemplate(createJedisPool());
 	
